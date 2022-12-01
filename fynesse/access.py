82db9af9c4616e525c37b0975ad9e5c73106655f
Fabien <fb531@cam.ac.uk>
2022-12-01 00:59:35 +0000

    testing prerun functions with colab

--- fynesse/access.py
+++ fynesse/access.py
@@ -11,10 +11,31 @@ import sqlite"""
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
+import yaml
+from ipywidgets import interact_manual, Text, Password
 
 
-def testing_func():
-    print("works!")
+@interact_manual(username=Text(description="Username:"), 
+                 password=Password(description="Password:"))
+def write_credentials(username, password):
+    with open("credentials.yaml", "w") as file:
+        credentials_dict = {'username': username, 
+                            'password': password}
+        yaml.dump(credentials_dict, file)
+
+def fetch_database_credentials():
+  database_details = {"url": "database-fb531.cgrre17yxw11.eu-west-2.rds.amazonaws.com", 
+                      "port": 3306}
+                      
+  with open("credentials.yaml") as file:
+    credentials = yaml.safe_load(file)
+  username = credentials["username"]
+  password = credentials["password"]
+  url = database_details["url"]
+  return username, password, url
+
+username, password, url = fetch_database_credentials()
+
 
 
 
